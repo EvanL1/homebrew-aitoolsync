96cc5db73168112f0f3f85f54792dae30ac2c775
lyf <liuyifanz.1996@gmail.com>
2026-03-18 11:48:09 +0800

    feat: add aisync formula v0.2.0

---
+++ Formula/aisync.rb
@@ -0,0 +1,30 @@
+class Aisync < Formula
+  desc "Sync AI agent configs across Claude Code, Codex, Gemini CLI, Cursor, Copilot, Windsurf, and Cline"
+  homepage "https://github.com/EvanL1/aitoolsync"
+  version "0.2.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/EvanL1/aitoolsync/releases/download/v#{version}/aisync-darwin-aarch64.tar.gz"
+    else
+      url "https://github.com/EvanL1/aitoolsync/releases/download/v#{version}/aisync-darwin-x86_64.tar.gz"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/EvanL1/aitoolsync/releases/download/v#{version}/aisync-linux-aarch64.tar.gz"
+    else
+      url "https://github.com/EvanL1/aitoolsync/releases/download/v#{version}/aisync-linux-x86_64.tar.gz"
+    end
+  end
+
+  def install
+    bin.install "aisync"
+  end
+
+  test do
+    system "#{bin}/aisync", "--version"
+  end
+end
